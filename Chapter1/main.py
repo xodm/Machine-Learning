5ac17c4ec199c7e1f87c82cbc50df394a4c1d15f
xodm <165731346+xodm@users.noreply.github.com>
2024-11-28 22:02:21 -0500

    Delete Chapter1 directory

--- Chapter1/main.py
+++
@@ -1,8 +0,0 @@
-#%matplotlib notebook
-import numpy as np
-import matplotlib.pyplot as plt
-import pandas as pd
-import mglearn
-from IPython.display import display
-import sklearn
-print("scikit-learn version: {}".format(sklearn.__version__))
